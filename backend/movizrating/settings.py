206c9e7ea6dc191eb9f143e42937a70f057596af
litesh1998 <49069637+litesh1998@users.noreply.github.com>
2021-06-27 11:58:08 +0530

    Connected django container with Postgres container

--- backend/movizrating/settings.py
+++ backend/movizrating/settings.py
@@ -96,8 +96,11 @@ WSGI_APPLICATION = 'movizrating.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'movieRater',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
     }
 }
 
